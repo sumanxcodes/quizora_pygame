794ca9dbc74528bec189172d23bd31cc87d999cd
Indusara <105542604@student.swin.edu.au>
2024-10-11 16:33:09 +0530

    added  mcq and question type configuration functionality

--- src/games/drag_n_drop.py
+++ src/games/drag_n_drop.py
@@ -1,11 +1,7 @@
-from asyncio import current_task
-from gettext import textdomain
 from tkinter import messagebox
 import tkinter as tk
 
 import pygame, sys
-from pygame.locals import *
-import time
 
 pygame.init()
 red = (255, 0, 0)
@@ -21,28 +17,27 @@ magenta = (255, 0, 255)
 gold = (255, 215, 0)
 sky_blue = (135, 206, 235)
 
+MCQ=0
+DRAG_N_DROP=1
+FILL_BLANKS=2
 
 X_DIST = 400
 Y_DIST = 400
-font = pygame.font.SysFont("Arial", 16)
+font = pygame.font.SysFont("Arial", 18)
 screen = pygame.display.set_mode((X_DIST, Y_DIST))
 screen.fill((255, 100, 100))
 pygame.display.set_caption("Hello")
-# canvas = pygame.Surface((X_DIST, Y_DIST))
-# canvas.fill((255, 255, 0, 100))
-# screen.blit(canvas, (0, 0))
+
 
 class GameScreen:
     def __init__(self, color=(255, 255, 0, 100), X_DIST=400, Y_DIST=400):
         self.screen=pygame.Surface((X_DIST, Y_DIST))
         self.screen.fill(color)
         self.color=color
-        print(color)
 
 
-
-    def add_questions(self, question, answerBox, answers,correctIndex):
-        question1 = Question(question, self.screen)
+    def add_questions(self, question, answerBox, answers,correctIndex, qtype):
+        question1 = Question(question, self.screen,qtype)
         question1.addAnswerBox(answerBox)
         counter=0
         for ans in answers:
@@ -53,6 +48,7 @@ class GameScreen:
             counter+=1
         self.question = question1
 
+
 class Game:
     def __init__(self):
         self.screens = []
@@ -60,11 +56,12 @@ class Game:
         self.currentScreen = None
         self.totalMarks=0
 
-    def addScreen(self, question, ansBox,answers,correctIndex, color=(255, 255, 0, 200), X_DIST=400, Y_DIST=400):
-        print(color)
+    def addScreen(self, question, ansBox,answers,correctIndex,qtype, color=(255, 255, 0, 200), X_DIST=400, Y_DIST=400):
+        # print(color)
         gameScreen= GameScreen( color, X_DIST, Y_DIST)
+        gameScreen.add_questions(question, ansBox,answers,correctIndex,qtype)
         self.screens.append(gameScreen)
-        gameScreen.add_questions(question, ansBox,answers,correctIndex)
+
         if len(self.screens) == 1:  # Set the first screen as the current screen
             self.currentScreen = self.screens[0].screen
             self.nextButton = Button(330, 360, "Next", self.screens[len(self.screens) - 1].screen)
@@ -101,8 +98,9 @@ class Game:
             pygame.display.update()
 
     def handle_event(self, event):
-        for screen in self.screens:
-            screen.question.handle_event(event)
+        self.screens[self.currentScreenId].question.handle_event(event)
+        # for screen in self.screens:
+        #     screen.question.handle_event(event)
         self.nextButton.rectangle.create_rect_with_text(self.currentScreen)
         self.backButton.rectangle.create_rect_with_text(self.currentScreen)
         if (self.nextButton.handle_click(event)):
@@ -116,11 +114,8 @@ class Game:
             print(screen)
 
 
-
-
 class Rectangle:
     def __init__(self, x, y, width, height, boader, text, r_color, t_color):
-        print("initializing")
         self.rect = pygame.Rect(x, y, width, height)
         self.rect_color = r_color
         self.text_color=t_color
@@ -153,13 +148,18 @@ class Rectangle:
             self.x = dx
             self.y = dy
 
-    def _enable_dragging(self, event, questionRectBoxes=[]):
+    def _enable_dragging(self, event,questionType, questionRectBoxes=[]):
+        clickedRect=None #to get clicked rectangle in question
         questionBoxes=[]
         for Rect in questionRectBoxes:
             questionBoxes.append(Rect.rect)
 
         if event.type == pygame.MOUSEBUTTONDOWN:
             self.drag= self.rect.collidepoint(event.pos)
+            if self.drag :
+                clickedRect=self.rect
+            if questionType==MCQ:
+                self.drag= False
         if event.type == pygame.MOUSEBUTTONUP:
             collideIndex = self.rect.collidelist(questionBoxes)
             if collideIndex ==-1:
@@ -167,15 +167,17 @@ class Rectangle:
             else:
                 self._move(questionBoxes[collideIndex].x, questionBoxes[collideIndex].y)
             self.drag= False
+        return clickedRect
 
-    def dragRect(self, event, answerBoxes):
-        self._enable_dragging(event,answerBoxes)
+    def dragRect(self, event, answerBoxes,questionType):
+        clickedRect= self._enable_dragging(event,questionType,answerBoxes)
         self._move(pygame.mouse.get_pos()[0], pygame.mouse.get_pos()[1])
+        return clickedRect
 
 
     def reset_moves(self):
         self.drag =True
-        print("moving to",self.initialPosition[0], self.initialPosition[1])
+        # print("moving to",self.initialPosition[0], self.initialPosition[1])
         self._move(self.initialPosition[0], self.initialPosition[1])
         self.drag=False
 
@@ -186,12 +188,17 @@ class Button:
         self.screen= canvas
     def handle_click(self, event):
         if event.type == pygame.MOUSEBUTTONDOWN:
-            print("clicked",event)
             return  self.rectangle.rect.collidepoint(event.pos)
         return False
-#
+
+############################################################################################
+############################################################################################
+############################################################################################
+############################################################################################
+############################################################################################
+
 class Question:
-    def __init__(self, questionRect, screen):
+    def __init__(self, questionRect, screen, qType):
         self.answerAdded = False
         self.questionRect=questionRect
         self.answerCorrect=False
@@ -200,11 +207,13 @@ class Question:
         self.screen=screen
         self.checkButton = Button(120, 259, "check", self.screen)
         self.resetButton = Button(190, 259, "reset", self.screen)
+        self.questionType=qType
+        self.clickedAnswer=None
 
-    def addAnswer(self, answer,isTrue=False):
+    def addAnswer(self, answer,isAnswer=False):
         answer.create_rect_with_text(self.screen)
         self.answerSet.append(answer)
-        if isTrue:
+        if isAnswer:
             self.correct_answer_index= len(self.answerSet)-1
 
     def addAnswerBox(self,answer_box):
@@ -219,26 +228,39 @@ class Question:
         if (self.checkButton.handle_click(event)):
             self.handle_check_button_submit()
         if(self.resetButton.handle_click(event)):
-            self.handle_reset_button_submit()
-
+            self.handle_reset_button()
 
         for ans in self.answerSet:
             if (not self.answerAdded):
-                ans.dragRect(event, [self.answer_box])
+                clickedRect= ans.dragRect(event, [self.answer_box], self.questionType)
+                if clickedRect:
+                    self.clickedAnswer=clickedRect
+                    print(self.clickedAnswer.width)
             ans.create_rect_with_text(self.screen)
 
+        if self.clickedAnswer and  self.questionType==MCQ:
+            r=Rectangle(self.clickedAnswer.x-2,self.clickedAnswer.y-2,self.clickedAnswer.width+4,self.clickedAnswer.height+4,1,"",black, red)
+            r.create_rect_with_text(self.screen)
 
     def handle_check_button_submit(self):
         self.answerAdded=True
-        if( self.answer_box.rect.colliderect(self.answerSet[self.correct_answer_index])):
-            self.showMessage("WOW", "Fantastic Job!")
-            self.answerCorrect=True
-        else:
-            self.showMessage("Try Again", "It is not !")
+        if self.questionType== DRAG_N_DROP:
+            if( self.answer_box.rect.colliderect(self.answerSet[self.correct_answer_index])):
+                self.showMessage("WOW", "Fantastic Job!")
+                self.answerCorrect=True
+            else:
+                self.showMessage("Try Again", "It is not !")
+        elif self.questionType == MCQ:
+            if (self.clickedAnswer.colliderect(self.answerSet[self.correct_answer_index])):
+                self.showMessage("WOW", "Fantastic Job!")
+                self.answerCorrect = True
+            else:
+                self.showMessage("Try Again", "It is not !")
 
-    def handle_reset_button_submit(self):
+    def handle_reset_button(self):
         self.answerCorrect = False
         self.answerAdded=False
+        self.clickedAnswer = None
         print("reset")
         for ans in self.answerSet:
             ans.reset_moves()
@@ -261,6 +283,7 @@ def keystroke(game):
 
 #############################################################
 #############################################################
+#############################################################
 #################### SARA ###################################
 #############################################################
 #############################################################
@@ -269,24 +292,24 @@ def keystroke(game):
 game1= Game()
 
 
-question=Rectangle(15, 80, 210, 30, 0, "Drag and fill the space  2, ___ , 6", yellow, blue)
+question=Rectangle(0, 80, 390, 30, 0, "Select the suitable answer to the space  2, ___ , 6", yellow, blue)
 ansBox=Rectangle(180, 80, 30, 30, 1, "", yellow, blue)
 answers=[Rectangle(150, 180, 20, 20, 0, "1", magenta, black),
             Rectangle(180, 180, 20, 20, 0, "2", magenta, black),
             Rectangle(210, 180, 20, 20, 0, "3", magenta, black),
             Rectangle(240, 180, 20, 20, 0, "4", magenta, black)]
 correctIndex=3
-game1.addScreen(question,ansBox, answers, correctIndex, green)
+game1.addScreen(question,ansBox, answers, correctIndex,MCQ, green)
 
 
-question=Rectangle(15, 15, 180, 30, 0, "Hi Mr. where are You?", yellow, blue)
-ansBox=Rectangle(200, 15, 180, 30, 1, "", red, blue)
-answers=[Rectangle(10, 180, 75, 30, 1, "Melbourne", blue, red),
-            Rectangle(90, 180, 75, 30, 1, "Colombo", blue, red),
-            Rectangle(170, 180, 75, 30, 1, "Malabe", blue, red),
-            Rectangle(250, 180, 75, 30, 1, "Matara", blue, red)]
-correctIndex=0
-canvas=game1.addScreen(question,ansBox, answers, correctIndex, blue)
+question1=Rectangle(15, 15, 180, 30, 0, "Hi Mr. where are You?", green, blue)
+ansBox1=Rectangle(200, 15, 180, 30, 1, "", red, blue)
+answers1=[Rectangle(11, 180, 75, 30, 0, "Melbourne", blue, red),
+            Rectangle(91, 180, 75, 30, 0, "Colombo", blue, red),
+            Rectangle(171, 180, 75, 30, 0, "Malabe", blue, red),
+            Rectangle(251, 180, 75, 30, 0, "Matara", blue, red)]
+correctIndex1=0
+canvas=game1.addScreen(question1,ansBox1, answers1, correctIndex1,DRAG_N_DROP, green)
 
 
 while True:
